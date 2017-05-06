cdc09b6a110187ff15cf078999933fa8d2b6048c
Jonathan Slate <jslate@patientslikeme.com>
2017-05-06 14:59:33 -0400

    use thor!

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@
 source "https://rubygems.org"
 
 gem "slim"
-# gem 'rb-readline'
-# gem 'pry'
+gem 'thor'
--- generate.rb
+++ generate.rb
@@ -1,55 +1,94 @@
 require 'slim'
 require 'ostruct'
 require 'json'
+require 'thor'
 require './app_data'
 require './image'
 
-`mkdir -p public/svgs`
-`mkdir -p public/pngs/small`
-`mkdir -p public/pngs/medium`
-`mkdir -p public/pngs/large`
+class AodCli < Thor
 
-`node genimages.js`
-
-`cp svgs/*.svg public/svgs/`
-`cp pencil_shorter_in_fist-300px.png public/`
-
-layout_contents = File.open("views/layout.slim", "rb").read
-layout = Slim::Template.new { layout_contents }
-
-# index
-contents = File.open("views/index.slim", "rb").read
-contents = Slim::Template.new { contents }.render(OpenStruct.new(images: AppData::IMAGES))
-File.open("public/index.html", 'w') do |output|
-  output.write layout.render(OpenStruct.new(
-    png_server_path: '/pencil_shorter_in_fist-300px.png',
-    description: "High quality vector graphics with free and open license agreements that you can use for resistance activities.",
-    server_html_path: "/",
-    page: 'index',
-    images: AppData::IMAGES)) { contents }
-end
-
-# about
-contents = File.open("views/about.slim", "rb").read
-contents = Slim::Template.new { contents }.render(OpenStruct.new(images: AppData::IMAGES))
-File.open("public/about.html", 'w') do |output|
-  output.write layout.render(OpenStruct.new(
-    png_server_path: '/pencil_shorter_in_fist-300px.png',
-    description: "High quality vector graphics with free and open license agreements that you can use for resistance activities.",
-    server_html_path: "/about.html",
-    page: 'about',
-    images: AppData::IMAGES)) { contents }
-end
-
-AppData::IMAGES.map{|h|Image.new(h)}.each do |image|
-  contents = File.open("views/image_page.slim", "rb").read
-
-  contents = Slim::Template.new { contents }.render(image)
-  File.open(image.full_html_path, 'w') do |output|
-    output.write layout.render(image) { contents }
+  desc 'create_public_dirs', 'Create the required directories in public'
+  def create_public_dirs
+    `mkdir -p public/svgs`
+    `mkdir -p public/pngs/small`
+    `mkdir -p public/pngs/medium`
+    `mkdir -p public/pngs/large`
   end
+
+  desc 'generate_images', 'Generate small, medium, and large PNG images for each SVG'
+  def generate_images
+    `node genimages.js`
+  end
+
+  desc 'generate_index', 'Generate the index page'
+  def generate_index
+    render_static_page('index', '/')
+  end
+
+  desc 'generate_about', 'Generate the about page'
+  def generate_about
+    render_static_page('about', '/about.html')
+  end
+
+  desc 'generate_image_pages', 'Generate an HTML page for each SVG image'
+  def generate_image_pages
+    AppData::IMAGES.map{|h|Image.new(h)}.each do |image|
+      slim = File.open("views/image_page.slim", "rb").read
+      contents = Slim::Template.new { slim }.render(image)
+      File.open(image.full_html_path, 'w') do |output|
+        output.write layout.render(image) { contents }
+      end
+    end
+  end
+
+  desc 'webpack', 'Run webpack to bundle up the Javascript and SCSS'
+  def webpack
+    `webpack`
+  end
+
+  desc 'build', 'Run all the generators and webpack'
+  def build
+    create_public_dirs
+    generate_images
+    generate_index
+    generate_about
+    generate_image_pages
+    webpack
+    copy_misc_stuff_to_public
+  end
+
+  desc 'sync_to_s3', 'Sync everythign to S3. THIS MEANS LIVE.'
+  def sync_to_s3
+    Dir.chdir('public') do
+      `aws s3 sync . s3://artofresistance.design --acl public-read`
+    end
+  end
+
+  private
+
+    def render_static_page(name, server_path)
+      slim = File.open("views/#{name}.slim", "rb").read
+      contents = Slim::Template.new { slim }.render(OpenStruct.new(images: AppData::IMAGES))
+      File.open("public/#{name}.html", 'w') do |output|
+        output.write layout.render(OpenStruct.new(
+          png_server_path: '/pencil_shorter_in_fist-300px.png',
+          description: "High quality vector graphics with free and open license agreements that you can use for resistance activities.",
+          server_html_path: server_path,
+          page: name)) { contents }
+      end
+    end
+
+    def copy_misc_stuff_to_public
+      `cp svgs/*.svg public/svgs/`
+      `cp pencil_shorter_in_fist-300px.png public/`
+    end
+
+    def layout
+      return @layout if @layout
+      layout_contents = File.open("views/layout.slim", "rb").read
+      @layout = Slim::Template.new { layout_contents }
+    end
+
 end
 
-`webpack`
-
-# aws s3 sync . s3://artofresistance.design --acl public-read
+AodCli.start(ARGV)
